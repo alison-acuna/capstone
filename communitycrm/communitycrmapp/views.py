2addc5c3e161c80ae1c308d32ad74665ec4ec0ff
alison-acuna <aacuna136@gmail.com>
2016-05-31 20:13:42 -0700

    Basic search functions added

--- communitycrm/communitycrmapp/views.py
+++ communitycrm/communitycrmapp/views.py
@@ -29,6 +29,12 @@ def display(request):
         'members': members
     })
 
+def member_item(request, id):
+    member = Member.objects.get(pk=id)
+    return render(request, 'communitycrmapp/member.html', {
+        'member': member
+    })
+
 def search_by_name(request):
     if request.method == "POST":
         search_id = request.POST.get('namequery')
@@ -42,9 +48,74 @@ def search_by_name(request):
     else:
         return render(request, 'communitycrmapp/searchpage.html')
 
+def search_by_volunteer(request):
+    if request.method == "POST":
+        search_id = request.POST.get('volunteerquery')
+        try:
+            member_by_volunteer = Member.objects.filter(polynteer__exact= search_id)
+            return render(request, 'communitycrmapp/results.html', {
+                'member_by_volunteer': member_by_volunteer})
+        except Member.DoesNotExist:
+            return HttpResponse("no such user")
+    else:
+        return render(request, 'communitycrmapp/searchpage.html')
 
-def member_item(request, id):
-    member = Member.objects.get(pk=id)
-    return render(request, 'communitycrmapp/member.html', {
-        'member': member
-    })
+def search_by_host(request):
+    if request.method == "POST":
+        search_id = request.POST.get('hostquery')
+        try:
+            member_by_host = Member.objects.filter(eventshosted__gte= search_id)
+            return render(request, 'communitycrmapp/results.html', {
+                'member_by_host': member_by_host})
+        except Member.DoesNotExist:
+            return HttpResponse("no such user")
+    else:
+        return render(request, 'communitycrmapp/searchpage.html')
+
+def search_by_ohb(request):
+    if request.method == "POST":
+        search_id = request.POST.get('ohbquery')
+        try:
+            member_by_ohb = Member.objects.filter(ohb__exact= search_id)
+            return render(request, 'communitycrmapp/results.html', {
+                'member_by_ohb': member_by_ohb})
+        except Member.DoesNotExist:
+            return HttpResponse("no such user")
+    else:
+        return render(request, 'communitycrmapp/searchpage.html')
+
+def search_by_lt(request):
+    if request.method == "POST":
+        search_id = request.POST.get('ltquery')
+        try:
+            member_by_lt = Member.objects.filter(launchteam__exact= search_id)
+            return render(request, 'communitycrmapp/results.html', {
+                'member_by_lt': member_by_lt})
+        except Member.DoesNotExist:
+            return HttpResponse("no such user")
+    else:
+        return render(request, 'communitycrmapp/searchpage.html')
+
+def search_by_fb(request):
+    if request.method == "POST":
+        search_id = request.POST.get('fbquery')
+        try:
+            member_by_fb = Member.objects.filter(fbgroupmember__exact= search_id)
+            return render(request, 'communitycrmapp/results.html', {
+                'member_by_fb': member_by_fb})
+        except Member.DoesNotExist:
+            return HttpResponse("no such user")
+    else:
+        return render(request, 'communitycrmapp/searchpage.html')
+
+def donor_search(request):
+    if request.method == "POST":
+        search_id = request.POST.get('donationquery')
+        try:
+            donor = Member.objects.filter(donationtotalammount__gte= search_id)
+            return render(request, 'communitycrmapp/results.html', {
+                'donor': donor})
+        except Member.DoesNotExist:
+            return HttpResponse("no such user")
+    else:
+        return render(request, 'communitycrmapp/searchpage.html')
